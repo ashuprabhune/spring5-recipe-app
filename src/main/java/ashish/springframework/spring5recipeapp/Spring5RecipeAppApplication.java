519c1dc0a7e809af7c20fe375e68a3e5f3701a62
Ashish Prabhune <ashuprabhune@gmail.com>
2020-01-27 12:38:10 -0500

    Added Many to Many relationship

--- src/main/java/ashish/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/ashish/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -2,6 +2,8 @@ package ashish.springframework.spring5recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
