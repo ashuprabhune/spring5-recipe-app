bf09a17872dfcf3bc88e0ac93701b8652b68aa87
Ashish Prabhune <ashuprabhune@gmail.com>
2020-07-09 17:54:11 -0400

    Added Integration checks

--- src/test/java/ashish/springframework/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/ashish/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -16,6 +20,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -51,5 +58,14 @@ class IndexControllerTest {
         verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
         Set<Recipe> checkSet = argumentCaptor.getValue();
         assertEquals(2,checkSet.size());
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 }
---
+++ src/test/java/ashish/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,32 @@
+package ashish.springframework.spring5recipeapp.repositories;
+
+import ashish.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+    @BeforeEach
+    void setUp() {
+
+    }
+
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> uonOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon",uonOptional.get().getDescription());
+    }
+}
