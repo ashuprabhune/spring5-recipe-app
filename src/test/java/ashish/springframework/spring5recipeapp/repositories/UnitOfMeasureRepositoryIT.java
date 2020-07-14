775aa044033d3c4c4cbe0758dbfbc02c4cbcabfd
Ashish Prabhune <ashuprabhune@gmail.com>
2020-07-14 19:52:51 -0400

    Fixing error in CircleCI builds

--- src/test/java/ashish/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/ashish/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-class UnitOfMeasureRepositoryIT {
+public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
